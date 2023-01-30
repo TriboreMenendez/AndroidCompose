b49b07725990bc1ffc40b42832fd6209edbd97cb
TriboreMenendez <ddnikulinm@gmail.com>
2023-01-30 16:51:06 +0500

    COMPOSE-1 the basic layout for the message card has been created

--- app/src/main/java/tribore/android/firstcomposetutorial/MainActivity.kt
+++ app/src/main/java/tribore/android/firstcomposetutorial/MainActivity.kt
@@ -3,41 +3,55 @@ package tribore.android.firstcomposetutorial
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.*
+import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
-import tribore.android.firstcomposetutorial.ui.theme.FirstComposeTutorialTheme
+import androidx.compose.ui.unit.dp
+import tribore.android.firstcomposetutorial.domain.Message
+
+val testMessage = Message(
+    author = "Tribore Menendez",
+    body = "Hello, men!"
+)
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            FirstComposeTutorialTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colors.background
-                ) {
-                    Greeting("Android")
-                }
-            }
+            MessageCard(testMessage)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun MessageCard(msg: Message) {
+    Row(modifier = Modifier.padding(all = 8.dp)) {
+        Image(
+            painter = painterResource(R.drawable.avatar_tribore),
+            contentDescription = "Contact profile picture",
+            modifier = Modifier
+                .size(40.dp)
+                .clip(CircleShape)
+        )
+        
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column {
+            Text(text = msg.author)
+            Spacer(modifier = Modifier.width(8.dp))
+            Text(text = msg.body)
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
-fun DefaultPreview() {
-    FirstComposeTutorialTheme {
-        Greeting("Android")
-    }
+fun PreviewMessageCard() {
+    MessageCard(testMessage)
 }
---
+++ app/src/main/java/tribore/android/firstcomposetutorial/domain/Message.kt
@@ -0,0 +1,9 @@
+package tribore.android.firstcomposetutorial.domain
+
+/**
+ * Created by MaksimNikulin on 30.01.2023
+ */
+data class Message(
+    val author: String,
+    val body: String
+)
