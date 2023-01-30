ade9944e97ec4666a2074dab0e93d35ea400a692
TriboreMenendez <ddnikulinm@gmail.com>
2023-01-30 17:28:52 +0500

    COMPOSE-1 added material design theme

--- app/src/main/java/tribore/android/firstcomposetutorial/MainActivity.kt
+++ app/src/main/java/tribore/android/firstcomposetutorial/MainActivity.kt
@@ -1,11 +1,15 @@
 package tribore.android.firstcomposetutorial
 
+import android.content.res.Configuration
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
@@ -14,6 +18,7 @@ import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import tribore.android.firstcomposetutorial.domain.Message
+import tribore.android.firstcomposetutorial.ui.theme.FirstComposeTutorialTheme
 
 val testMessage = Message(
     author = "Tribore Menendez",
@@ -24,7 +29,11 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MessageCard(testMessage)
+            FirstComposeTutorialTheme {
+                Surface {
+                    MessageCard(testMessage)
+                }
+            }
         }
     }
 }
@@ -38,20 +47,42 @@ fun MessageCard(msg: Message) {
             modifier = Modifier
                 .size(40.dp)
                 .clip(CircleShape)
+                .border(width = 1.5.dp, color = MaterialTheme.colors.secondary, CircleShape)
         )
-        
+
         Spacer(modifier = Modifier.width(8.dp))
 
         Column {
-            Text(text = msg.author)
-            Spacer(modifier = Modifier.width(8.dp))
-            Text(text = msg.body)
+            Text(
+                text = msg.author,
+                color = MaterialTheme.colors.secondaryVariant,
+                style = MaterialTheme.typography.subtitle2
+            )
+
+            Spacer(modifier = Modifier.width(4.dp))
+
+            Surface(shape = MaterialTheme.shapes.medium, elevation = 1.dp) {
+                Text(
+                    text = msg.body,
+                    style = MaterialTheme.typography.subtitle2,
+                    modifier = Modifier.padding(all = 4.dp)
+                )
+            }
         }
     }
 }
 
-@Preview(showBackground = true)
+@Preview(name = "Light Mode")
+@Preview(
+    uiMode = Configuration.UI_MODE_NIGHT_YES,
+    showBackground = true,
+    name = "Dark Mode")
 @Composable
 fun PreviewMessageCard() {
-    MessageCard(testMessage)
+    FirstComposeTutorialTheme {
+        Surface {
+            MessageCard(testMessage)
+        }
+    }
+
 }
